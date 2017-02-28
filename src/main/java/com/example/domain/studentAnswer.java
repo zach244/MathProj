2a489aec59bb4057e34edf7211ddad8cf0f58746
zach244 <zacharyyoung244@gmail.com>
2017-02-27 23:55:25 -0700

    Added start of studentAnswer
    started The new Entity

--- src/main/java/com/example/domain/studentAnswer.java
+++ src/main/java/com/example/domain/studentAnswer.java
@@ -5,12 +5,12 @@ import javax.persistence.*;
 /**
  * Created by zach on 2/27/2017.
  */
-@Entity
+//@Entity
 public class studentAnswer implements java.io.Serializable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name="studentAnswer_id")
-    private int id;
-    private int answer;
-    
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @Column(name="studentAnswer_id")
+//    private int id;
+//    private int answer;
+
 }

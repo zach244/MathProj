d8299ff1465444e7ead6d059541afbb14807b16a
zach244 <zacharyyoung244@gmail.com>
2017-04-03 23:56:10 -0600

    can't get api call to work

--- src/test/java/com/example/service/TestServiceTest.java
+++ src/test/java/com/example/service/TestServiceTest.java
@@ -1,38 +1,41 @@
-package com.example.service;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
-
-import static org.junit.Assert.*;
-
-/**
- * Created by ZACH on 3/24/2017.
- */
-@RunWith(MockitoJUnitRunner.class)
-public class TestServiceTest {
-
-
-    @Before
-    public void setUp() throws Exception {
-
-    }
-
-    @After
-    public void tearDown() throws Exception {
-
-    }
-
-    @Test
-    public void createTest() throws Exception {
-
-    }
-
-    @Test
-    public void findAll() throws Exception {
-
-    }
-
-}
+//package com.example.service;
+//
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.mockito.runners.MockitoJUnitRunner;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//import static org.junit.Assert.*;
+//
+///**
+// * Created by ZACH on 3/24/2017.
+// */
+//@RunWith(MockitoJUnitRunner.class)
+//public class TestServiceTest {
+//    private Test test;
+//    private TestService testService;
+//
+//
+//    @Before
+//    public void setUp() throws Exception {
+//    test = mock(com.example.domain.Test.class);
+//    }
+//
+//    @After
+//    public void tearDown() throws Exception {
+//
+//    }
+//
+//    @Test
+//    public void createTest() throws Exception {
+//
+//    }
+//
+//    @Test
+//    public void findAll() throws Exception {
+//
+//    }
+//
+//}

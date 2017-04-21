0c55c96a38e8c641ba415414661505467d0b1633
zach244 <zacharyyoung244@gmail.com>
2017-04-20 18:31:52 -0600

    Issue with question modal, need to understand how it works, may need to duplicate the same functions but for the new modal.

--- src/main/resources/static/js/teachtestdash.js
+++ src/main/resources/static/js/teachtestdash.js
@@ -3,8 +3,8 @@
  */
 
 // Get the modal
-
-
+//#TODO Issue with question modal, need to understand how it works, may need to duplicate the same functions but for the new modal.
+//need to figure out how a modal works
 var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
@@ -32,9 +32,25 @@ window.onclick = function (event) {
 $("#btnDel").click(function () {
     alert(this.id);
 });
+
 function getPropId(inputID) {
     alert(inputID);
-    $.get("http://localhost:8080/questions/" + inputID, function (data, status) {
-        document.getElementById("mainDisplay").innerHTML = JSON.stringify(data);// doesn't work
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080//tests/" + inputID,
+        dataType: 'json',
+        success: function (response) {
+            var trHTML = '';
+            $.each(response, function (i, item) {
+                trHTML += '<tr><td>' + item.var1 + '</td><td>' + item.var2 + '</td><td>' + item.questionText + '</td></tr>';
+            });
+            $("#modalQuestionTable").append(trHTML);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert(textStatus);
+            alert(errorThrown);
+        }
     });
+
 };
+
